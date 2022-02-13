09d3956c09ac22e023fa6817007cb17950295283
JasonLuuk <96975358+JasonLuuk@users.noreply.github.com>
2022-02-13 15:56:07 +0000

    Update 1006.c

--- Lab1 (1006,1013,1085)/1006.c
+++ Lab1 (1006,1013,1085)/1006.c
@@ -1,6 +1,1 @@
-#include<stdio.h>
-int main() {
-    char a,b,c;
-    scanf(" %c %c %c",&a, &b, &c);
-    printf("%hd", (int)(a+b+c)-144);
-}
+main(){char a,b,c;scanf("%c %c %c",&a,&b,&c);printf("%d",a+b+c-144);}
