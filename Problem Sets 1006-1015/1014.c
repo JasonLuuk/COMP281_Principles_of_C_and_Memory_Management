67eca1d21bc34f5af6271c2481f6a18ea2812e3e
JasonLuuk <96975358+JasonLuuk@users.noreply.github.com>
2022-02-14 13:39:14 +0000

    Update 1014.c

--- Problem Sets 1006-1015/1014.c
+++ Problem Sets 1006-1015/1014.c
@@ -1,12 +1,1 @@
-#include<stdio.h>
-
-int main() {
-    float a, b, c, d;
-    scanf("%f%f", &a, &b);
-    while (a <= b) {
-        c += 3.14 * a * a;
-        d += 2 * 3.14 * a;
-        ++a;
-    }
-    printf("%.3f\n%.3f", c, d);
-}
+Presented after the 25th
