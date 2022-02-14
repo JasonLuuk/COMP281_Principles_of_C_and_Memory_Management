9d77d2d8f1fcda21d46fe2417e5ab9c7f4d9d27d
JasonLuuk <96975358+JasonLuuk@users.noreply.github.com>
2022-02-14 13:40:00 +0000

    Update 1018.c

--- Problem Sets 1016-1025/1018.c
+++ Problem Sets 1016-1025/1018.c
@@ -1,12 +1,1 @@
-#include<stdio.h>
-
-void R() {
-    char c;
-    scanf("%c", &c);
-    if (c != '\0') {
-        R();
-        printf("%c", c);
-    }
-}
-
-int main() { R(); }
+Presented after the 25th
